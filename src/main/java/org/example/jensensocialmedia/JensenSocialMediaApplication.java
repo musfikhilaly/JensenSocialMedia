bee7486f6fca128f5a65685786cd9e7d3ba3fd66
musfikhilaly <musfikhilaly@gmail.com>
2026-01-12 14:48:48 +0100

    1

---
+++ src/main/java/org/example/jensensocialmedia/JensenSocialMediaApplication.java
@@ -0,0 +1,13 @@
+package org.example.jensensocialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JensenSocialMediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JensenSocialMediaApplication.class, args);
+    }
+
+}
