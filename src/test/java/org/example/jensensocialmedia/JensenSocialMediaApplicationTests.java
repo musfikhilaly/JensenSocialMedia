de49493014fb51ff528106c52d30033bf4c6ab40
YukiJanse <yuki.janse@gmail.com>
2026-01-30 22:03:29 +0100

    Revert "Modified github action file for portfolio"
    This reverts commit 70c43d8ac5f1c12e4fb58af68020ad222d423ff5.

---
+++ src/test/java/org/example/jensensocialmedia/JensenSocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jensensocialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JensenSocialMediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
